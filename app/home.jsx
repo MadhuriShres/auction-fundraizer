43c0b3f9ef4dd51a60ce923ce319969a1c6095ea
tripti <tripti@cloudfactory.com>
2017-03-02 14:19:47 +0545

    route added

---
+++ app/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class Home extends React.Component {
+  render() {
+   
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+export default Home;
--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'; 
+import  Home from './home.jsx';
 
-class App extends React.Component {
-  render () {
-    return <p> Hello Auction </p>;
-  }
+const Routes = () => {
+   
+    return (
+        <Router history={browserHistory}> 
+            <Route path = "/" >
+                <IndexRoute component={Home} />
+                 <Route path = "/Cloth" component={Home}  />
+
+            </Route>
+        </Router>
+    );
 }
+    
 
-render(<App/>, document.getElementById('app'));
+render(<Routes />, document.getElementById('app'));
