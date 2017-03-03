7e9b033ab4113ec72169f3e0acb8bcd3f2621131
tripti <tripti@cloudfactory.com>
2017-03-03 18:02:54 +0545

    removed unwanted lines

--- application/home.jsx
+++ application/home.jsx
@@ -3,17 +3,24 @@ import { render } from 'react-dom';
 import Header from './Header.jsx';
 import Container from './container.jsx';
 import Footer from './footer.jsx';
+import test from './skylight.jsx';
 
 class Home extends React.Component {
   render() {
    
     return (
       <div>
+        <section id="page">
           <div className="wrapper">
+              
             <div>  <Header /> </div>
+              <div> <test /> </div>
             <div> <Container /> </div> 
             <div> <Footer /> </div>
+         
+
          </div>
+        </section>
       </div>
     );
   }
