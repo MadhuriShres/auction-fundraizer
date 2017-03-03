d42c2934463221d3bd4af2d9a27b004b2e31acea
tripti <tripti@cloudfactory.com>
2017-03-03 18:01:33 +0545

    added proper text

--- application/footer.jsx
+++ application/footer.jsx
@@ -8,7 +8,12 @@ class footer extends React.Component {
    
     return (
       <div>
-       footer
+        <div className="line"></div>
+           <div className="footertext">My mission in life is not merely to survive, but to thrive; and to do so with some passion, some compassion, some humor, and some style. 
+</div>
+     
+           
+         
       </div>
     );
   }
