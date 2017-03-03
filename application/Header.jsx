62a20109ea3d4ab23caea330cbc38c9725bef94c
tripti <tripti@cloudfactory.com>
2017-03-03 18:00:16 +0545

    changed name

--- application/Header.jsx
+++ application/Header.jsx
@@ -6,20 +6,23 @@ class Header extends React.Component {
    
     return (
       <div>
-           <nav className="navbar navbar-default">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                    <a className="navbar-brand" href="#">WebSiteName</a>
-                    </div>
-                    <ul className="nav navbar-nav">
-                    <li className="active"><a href="#">Home</a></li>
-                    <li><a href="#">Page 1</a></li>
-                    <li><a href="#">Page 2</a></li>
-                    <li><a href="#">Page 3</a></li>
+             <header> 
+            
+                <hgroup>
+                    <h1>FundRaiser</h1>
+                   
+                </hgroup>
+                                
+                <nav className="clear"> 
+                    <ul>
+                        <li><a href="#article1">Clothes</a></li>
+                        <li><a href="#article2">Accessaries</a></li>
+                        <li><a href="#article3">Navigation Menu</a></li>
                     </ul>
-                </div>
-            </nav>
-        Hello
+                </nav>
+            
+            </header>
+      
       </div>
     );
   }
