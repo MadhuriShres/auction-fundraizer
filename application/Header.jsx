12537c92b08377d5d9f5cb11c7a54ba0e8546940
tripti <tripti@cloudfactory.com>
2017-03-06 18:49:51 +0545

    routing

--- application/Header.jsx
+++ application/Header.jsx
@@ -1,29 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
+import {  Link } from 'react-router'
+import SkyLight from 'react-skylight';
+
 
 class Header extends React.Component {
+
   render() {
-   
+    
+
     return (
       <div>
-             <header> 
-            
+          <header> 
                 <hgroup>
                     <h1>FundRaiser</h1>
-                   
                 </hgroup>
-                                
-                <nav className="clear"> 
-                    <ul>
-                        <li><a href="#article1">Clothes</a></li>
-                        <li><a href="#article2">Accessaries</a></li>
-                        <li><a href="#article3">Navigation Menu</a></li>
-                    </ul>
-                </nav>
-            
+                     <nav className="clear">
+                             <Link to={'/'}>  <button className="header"> Home</button> </Link>
+                             <Link to={'/Clothes'}>  <button className="header"> Clothes</button> </Link>
+                             <Link to={'/Accessaries'}>  <button className="header"> Accessaries</button> </Link>
+                        </nav>
             </header>
-      
-      </div>
+       </div>
     );
   }
 }
