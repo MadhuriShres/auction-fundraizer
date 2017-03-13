a774275bfd845d87d446df05ede4afa8860db1d8
MadhuriShres <madhurishres@gmail.com>
2017-03-13 15:51:26 +0545

    Use postgres instead of sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170312163334) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "auction_items", force: :cascade do |t|
     t.string   "image_url"
     t.string   "name"
