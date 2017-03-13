b11f22c270d5a8061ff7e52677362a2dae2590d0
MadhuriShres <madhurishres@gmail.com>
2017-03-13 13:28:38 +0545

    Add controller & model for auction item

---
+++ app/controllers/auction_items_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# Handles all action related to items
+class AuctionItemsController < ApplicationController
+  def index
+    category = params['category']
+    render json: category.present? ? AuctionItem.where(category: category) : AuctionItem.all
+  end
+
+  def show
+    render json: AuctionItem.find_by!(id: params['id'])
+  end
+
+  def update
+    update_params = permitted_params.merge(sold: true)
+    auction_item = AuctionItem.find_by!(id: params['id'])
+    render json: auction_item.update!(update_params)
+  end
+
+  private
+
+  def permitted_params
+    params.require(:sold_to)
+  end
+end
