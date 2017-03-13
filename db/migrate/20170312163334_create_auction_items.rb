5dad948c259d897638be13138d9a1cd9c540e49d
MadhuriShres <madhurishres@gmail.com>
2017-03-13 13:32:24 +0545

    Add migration file for auction items

---
+++ db/migrate/20170312163334_create_auction_items.rb
@@ -0,0 +1,15 @@
+class CreateAuctionItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :auction_items do |t|
+      t.string :image_url
+      t.string :name
+      t.string :description
+      t.integer :price
+      t.boolean :sold
+      t.string :sold_to
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
