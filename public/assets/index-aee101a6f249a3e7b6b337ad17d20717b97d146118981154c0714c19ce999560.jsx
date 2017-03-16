be618c0d1c8450ac2560b78d27fb0fe761498b19
MadhuriShres <madhurishres@gmail.com>
2017-03-16 17:00:13 +0545

    Add precompiled assets for heroku

---
+++ public/assets/index-aee101a6f249a3e7b6b337ad17d20717b97d146118981154c0714c19ce999560.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'; 
+import  Home from './component/home.jsx';
+import Faq from './component/faq.jsx';
+import Delivery from './component/delivery.jsx';
+import Size from './component/size.jsx';
+
+const Routes = () => {
+   
+    return (
+        <Router history={browserHistory}> 
+            <Route path = "/" >
+                <IndexRoute component={Home} />
+                 <Route path = "clothes"  component={Home}  />
+                 <Route path = "DecorativeItems"   component={Home}  />
+                  <Route path = "Bags" component={Home}  />
+                   <Route path = "SoftToys" component={Home}  />
+                    <Route path = "faq" component={Faq}  />
+                    <Route path = "delivery" component={Delivery}  />
+                    <Route path = "size" component={Size}  />
+
+            </Route>
+        </Router>
+    );
+}
+    
+
+render(<Routes />, document.getElementById('react-app'));
