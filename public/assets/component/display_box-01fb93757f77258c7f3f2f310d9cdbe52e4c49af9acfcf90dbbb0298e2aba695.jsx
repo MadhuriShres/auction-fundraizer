4ad7e5d8cf85ea8257e4f335ec86874d16c5da26
MadhuriShres <madhurishres@gmail.com>
2017-03-20 16:21:16 +0545

    Add precompiled assets for heroku

---
+++ public/assets/component/display_box-01fb93757f77258c7f3f2f310d9cdbe52e4c49af9acfcf90dbbb0298e2aba695.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import  ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+const DisplayBox = ({image, viewDetails,handleclicklikes}) =>
+  <div className ="box">
+    <img
+      src={image.image_url}
+      onClick={() => viewDetails(image)}
+    />
+    <div className="bidrate">{image.sold ? 'sold' : `NPR ${image.price}`}</div>
+    <ReactCSSTransitionGroup
+      transitionName="like"
+      transitionEnterTimeout={500}
+      transitionLeaveTimeout={500}
+    >
+      <span key={image.likes} className="likes-heart">{image.likes}</span>
+    </ReactCSSTransitionGroup>
+
+    <button
+      className="likes"
+     onClick={() => handleclicklikes(image)}
+    >
+      &hearts;
+      {image.likes}
+    </button>
+  </div>;
+
+export default DisplayBox;
