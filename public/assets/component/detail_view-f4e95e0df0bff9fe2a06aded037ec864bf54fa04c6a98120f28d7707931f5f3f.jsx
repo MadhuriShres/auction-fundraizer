c775150e13808ec9fff0258d05208db9c54640a6
MadhuriShres <madhurishres@gmail.com>
2017-03-20 20:27:44 +0545

    Add precompiled assets for heroku

---
+++ public/assets/component/detail_view-f4e95e0df0bff9fe2a06aded037ec864bf54fa04c6a98120f28d7707931f5f3f.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+const DetailView = (props) =>
+  <div className="row">
+    <div className="col-xs-6">
+      <img src={props.image.image_url}  className ="skylightimage img-responsive"/>
+    </div>
+    <div className="col-xs-6" >
+      <div className ="labeling" >
+        <div className='display-box-name'>{props.image.name}<br/></div>
+        NPR: {props.image.price} <br/><br/>
+        {props.image.sold ? 'Already Sold' :
+         <button
+          className="wannabuy"
+          id="buyid"
+          onClick={(evt) => props.handleShopClick(evt)}>
+          Shop
+        </button>
+        }
+
+        <br/>
+        <div id="emailaddresshidden" className="emailaddresshidden">
+          <form>
+            <div className="row">
+              <div className="col-xs-9">
+                <input
+                  type="email"
+                  placeholder="CloudFactory email"
+                  value={props.emailaddress}
+                  onChange={(evt) =>props.handleEmailChange(evt)}
+                  className="form-control"
+                  required
+                />
+              </div>
+              <div className="col-xs-2">
+                <button
+                  className="btn btn-primary"
+                  onClick={() => props.handleButtonClick(props.image)}
+                >
+                  Buy
+                </button>
+              </div>
+            </div>
+          </form>
+           
+        </div>
+      </div>
+    </div>
+  </div>;
+
+export default DetailView;
